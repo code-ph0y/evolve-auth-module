c30274e7c4246ca4baa8714d3aa9d33fb488e954
Ross Moroney <code.ph0y@googlemail.com>
2016-02-15 00:20:04 +0000

    Add Render and Security Template Helper

--- src/Controller/Shared.php
+++ src/Controller/Shared.php
@@ -11,4 +11,18 @@ class Shared extends BaseController
             return $this->redirectToRoute($this->getService('auth.security')->getRedirectRoute());
         }
     }
+    
+    /**
+     * Render a template
+     *
+     * @param  string $template The template to render
+     * @param  array $params The params to pass to the renderer
+     * @param  array $options Extra options
+     * @return string
+     */
+    protected function render($template, array $params = array(), array $options = array())
+    {
+        $options['helpers'][] = $this->getService('auth.security.templating.helper');
+        return parent::render($template, $params, $options);
+    }
 }
